6d34e32dd4f24c137fcdb3b3792fb5a462deb9d0
Kasule Edward <>
2022-11-05 07:12:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Same output different approach
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')\n")
+
+#2. Using the \n for newline
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')\n")
+
+#3. String Concatination
+print("Day 1 - python Print Function" + "\nThe function is declared like this:" + "\nprint('what to print')")
